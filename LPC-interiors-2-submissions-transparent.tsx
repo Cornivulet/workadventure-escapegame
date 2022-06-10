2270562300694b6dae42bbe3eee22c835404e7f1
KhalilSekhri <khalil.sekhri@gmail.com>
2022-06-10 16:07:23 +0200

    add script

---
+++ LPC-interiors-2-submissions-transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="LPC-interiors-2-submissions-transparent" tilewidth="32" tileheight="32" tilecount="832" columns="32">
+ <image source="../../../Downloads/LPC-interiors-2-submissions-transparent.png" width="1024" height="832"/>
+</tileset>
---
+++ work_adventure_tilesets_ag.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="work_adventure_tilesets_ag" tilewidth="32" tileheight="32" tilecount="481" columns="13">
+ <image source="../../../Downloads/work_adventure_tilesets_ag.png" width="416" height="1184"/>
+</tileset>
